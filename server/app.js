ec084dec648518d0c23c7337b0dc89f22e088431
refluster <refluster@gmail.com>
2017-02-17 07:31:16 +0900

    command for create certification

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
-var express = require("express");
-var app = express();
-
-var server = app.listen(3000, function(){
-    console.log("Node.js is listening to PORT:" + server.address().port);
-});
+var fs = require('fs'),
+    https = require('https'),
+    express = require('express'),
+    app = express();
 
 var io = require('socket.io')();
 var client = null;
@@ -13,9 +11,24 @@ io.on('connection', function(_client){
 });
 io.listen(3001);
 
-app.get("/api/light", function(req, res, next){
+app.get('/api/light', function(req, res, next){
 	console.log(req);
     res.json('res');
 	client.emit('event');
 });
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(3000, function(https) {
+    console.log('Node.js is listening');
+});
+
+app.get('/', function (req, res) {
+    res.header('Content-type', 'text/html');
+    return res.end('<h1>Hello, Secure World!</h1>');
+});
+
+
+
 
