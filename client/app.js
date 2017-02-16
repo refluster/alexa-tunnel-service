ccfe1adf93b349e445aca6c70562c1fe0b924c2d
refluster <refluster@gmail.com>
2017-02-16 23:44:03 +0900

    send event from server to client when web api is called

--- client/app.js
+++ client/app.js
@@ -1,6 +1,11 @@
 var socket = require('socket.io-client')('http://localhost:3001');
+//var socket = require('socket.io-client')('http://52.199.118.199:3001');
 socket.on('connect', function(){
 	console.log('connected');
 });
-socket.on('event', function(data){});
-socket.on('disconnect', function(){});
+socket.on('event', function(data){
+	console.log('event');
+});
+socket.on('disconnect', function(){
+	console.log('disconnected');
+});
