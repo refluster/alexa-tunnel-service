cd5ea84c5868f510717ef4b59b4fe36e6dae6603
refluster <refluster@gmail.com>
2017-02-16 23:43:56 +0900

    send event from server to client when web api is called

--- server/app.js
+++ server/app.js
@@ -5,14 +5,17 @@ var server = app.listen(3000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
+var io = require('socket.io')();
+var client = null;
+io.on('connection', function(_client){
+	console.log('connected');
+	client = _client;
+});
+io.listen(3001);
+
 app.get("/api/light", function(req, res, next){
 	console.log(req);
     res.json('res');
+	client.emit('event');
 });
 
-
-var io = require('socket.io')();
-io.on('connection', function(client){
-	console.log('connected');
-});
-io.listen(3001);
